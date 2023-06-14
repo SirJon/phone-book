21b070d68e1f9b0b887e986f918aa93b3390955c
Silin Evgenii <e.silin@avm-t.ru>
2023-06-14 17:58:43 +0300

    sort up down

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,12 +3,31 @@ import TableData from "./components/TableData/TableData";
 
 const Table = ({ data = [] }) => {
   const [userData, setUserData] = useState([]);
+  const [fieldState, setFieldState] = useState('');
+  const [returnValue, setReturnValue] = useState(-1);
+
   useEffect(() => {
     setUserData(data)
   }, [data]);
+
   const sortDate = (field) => {
-    const resultData = userData.concat().sort((a, b) => a[field] > b[field] ? 1 : -1);
+    const resultData = userData
+      .concat()
+      .sort((a, b) => {
+        if (field !== fieldState) {
+          setReturnValue(-1);
+          return a[field] > b[field]
+            ? 1
+            : -1
+        } else {
+          setReturnValue(returnValue * (-1))
+          return a[field] > b[field]
+            ? 1 * returnValue
+            : -1 * returnValue
+        }
+      });
     setUserData(resultData)
+    setFieldState(field);
   };
 
   return (
